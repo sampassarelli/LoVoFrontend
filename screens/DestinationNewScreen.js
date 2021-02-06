f9a6c86f47f70013d62591b2e826f158410b8a25
sampassarelli <passarelli.sam@gmail.com>
2021-02-05 23:38:22 -0600

    more screens and mapview

--- screens/DestinationNewScreen.js
+++ screens/DestinationNewScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Switch, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Switch, Text, View } from "react-native";
 import * as Yup from "yup";
 
 import Screen from "../components/Screen";
@@ -10,88 +10,128 @@ import colors from "../config/colors"
 import AppText from "../components/AppText";
 
 const validationSchema = Yup.object().shape({
-  name: Yup.string().required().label("Name"),
-  username: Yup.string().required().label("Username"),
-  email: Yup.string().required().email().label("Email"),
-  password: Yup.string().required().min(4).label("Password"),
+  // name: Yup.string().required().label("Name of Destination"),
+  // username: Yup.string().required().label("Username"),
+  // email: Yup.string().required().email().label("Email"),
+  // password: Yup.string().required().min(4).label("Password"),
 });
 
 function DestinationNewScreen() {
   const [visited, setVisited] = useState(false)
+  
+  {/* This const below is So that the Longitude and Latitude fields are always hidden */}
+  const hiddenFields = false
 
   return (
     <Screen style={styles.container}>
-      <Text style={styles.header}>Where to Next?</Text>
-      <AppForm
-        initialValues={{ 
-          address: "", 
-          name: "", 
-          category: "", 
-          visited: visited,
-          date_visited: "",
-          cost: 0,
-          attendees: "",
-          latitude: 0.0,
-          longitude: 0.0,
-           }}
-        onSubmit={(values) => console.log(values)}
-        validationSchema={validationSchema}
-      >
-        <AppFormField
-          autoCorrect={false}
-          icon="magnify"
-          name="google"
-          placeholder="GOOGLE SEARCH"
-        />
-        <AppFormField
-          autoCorrect
-          name="name"
-          placeholder="Name"
-        />
-         <AppFormField
-          autoCorrect
-          name="address"
-          placeholder="Address"
-        />
-        <AppFormField
-          autoCorrect
-          name="address"
-          placeholder="Category"
-        />
-        <AppText style={styles.visited}>
-          Have You Visited Here Yet?
-        </AppText>
-        <Switch 
-          style={styles.switch} 
-          value={visited}
-          onValueChange={newValue => setVisited(newValue)}
+      <ScrollView>
+        <Text style={styles.header}>Where to Next?</Text>
+        <AppForm
+          initialValues={{ 
+            address: "", 
+            name: "", 
+            category: "", 
+            visited: visited,
+            date_visited: "",
+            cost: 0,
+            attendees: "",
+            latitude: 0.0,
+            longitude: 0.0,
+            }}
+          onSubmit={(values) => console.log(values)}
+          validationSchema={validationSchema}
+        >
+          <AppFormField
+            autoCorrect={false}
+            icon="magnify"
+            name="google"
+            placeholder="GOOGLE SEARCH"
           />
-        <View>
-          {
-          visited ?
-          <View>
-            <Text style={styles.optional}>
-            Optional Fields Below
-            </Text>
-            <AppFormField
-              autoCorrect
-              name="address"
-              placeholder="Category"
+          <AppFormField
+            autoCorrect
+            name="name"
+            placeholder="Name"
+          />
+          <AppFormField
+            autoCorrect
+            name="address"
+            placeholder="Address"
+          />
+          <AppFormField
+            autoCorrect
+            name="address"
+            placeholder="Category"
+          />
+          <AppFormField
+            autoCorrect
+            name="comment"
+            placeholder="Memories, Comments, Notes, etc. "
+          />
+          <AppText style={styles.visited}>
+            Have You Visited Here Yet?
+          </AppText>
+          <Switch 
+            name="visited"
+            style={styles.switch} 
+            value={visited}
+            onValueChange={newValue => setVisited(newValue)}
             />
-            </View>
-            :
-            null
-          }
-        </View>
-        <SubmitButton title="Add Destination" />
-      </AppForm>
+          <View>
+            {
+            visited ?
+            <View>
+              <Text style={styles.optional}>
+              Optional Fields Below
+              </Text>
+              <AppFormField
+                autoCorrect
+                name="date"
+                placeholder="Date Visited - MM/DD/YYYY"
+              />
+              <AppFormField
+                autoCorrect
+                name="cost"
+                placeholder="Money Spent"
+              />
+              <AppFormField
+                autoCorrect
+                name="attendees"
+                placeholder="Attendees"
+              />
+              </View>
+              :
+              null
+            }
+          </View>
+          {/* This View Below is So that the Longitude and Latitude fields are always hidden */}
+          <View>
+            {
+              hiddenFields 
+              ? 
+              <View>
+                <AppFormField
+                  autoCorrect
+                  name="longitude"
+                />
+                <AppFormField
+                  autoCorrect
+                  name="latitude"
+                />
+              </View>
+              :
+              null
+            }
+          </View>
+          <SubmitButton title="Add Destination" onSubmit={() => console.log("Add Destination Button Touched")} />
+        </AppForm>
+      </ScrollView>
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
+    padding: 0,
   },
   header: {
     alignSelf: "center",
--- screens/MapHome.js
+++ screens/MapHome.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import { View, StyleSheet,Text } from 'react-native';
+import MapView from 'react-native-maps'
+import Marker from 'react-native-maps'
 
 import Screen from '../components/Screen'
 
 function MapHome(props) {
   return (
     <Screen>
-      <View style={styles.container}><Text>Hello From the Map Home Screen</Text></View>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          longitude: -87.629799,
+          latitude: 41.878113,
+          longitudeDelta: 0.4,
+          latitudeDelta: 0.8
+        }}
+      >
+        <Marker
+        coordinate
+        >
+         
+       
+          </Marker>
+      </MapView>
     </Screen>
+    // <Screen>
+    //   <View style={styles.container}><Text>Hello From the Map Home Screen</Text></View>
+    // </Screen>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     alignSelf: "center"
+  },
+  map:{
+    flex: 1,
+    marginTop: 0
   }
 });
 
